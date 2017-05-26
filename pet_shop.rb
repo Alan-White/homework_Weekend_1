dbd40c3a3cc4deda3794db1f89e8dae9c64f19be
Alan White <alanwhitey@gmail.com>
2017-05-26 12:57:06 +0100

    initial commit

---
+++ pet_shop.rb
@@ -0,0 +1,12 @@
+def pet_shop_name(title)
+return @pet_shop[:name]
+end
+
+def total_cash(money)
+  return @pet_shop[:admin][:total_cash]
+end
+
+# def add_or_remove_cash(deposit, number)
+#   return @pet_shop[:admin][:total_cash] + 10
+# end
+
