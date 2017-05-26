1ece323bb64ebf74686f2734ab4c71cc8d4313d8
Alan White <alanwhitey@gmail.com>
2017-05-26 13:50:38 +0100

    5 out of 15 functions passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -10,3 +10,10 @@ def add_or_remove_cash(receiver, amount)
   return @pet_shop[:admin][:total_cash] += amount
 end
 
+def add_or_remove_cash(payout, amount)
+  return @pet_shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(sales)
+  return @pet_shop[:admin][:pets_sold]
+end
