5610ecf26a86f62daa7c551da04f7710352bc92d
Alan White <alanwhitey@gmail.com>
2017-05-28 10:18:36 +0100

    15 of 15 plus 2 optional functions passed test.

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,21 +1,18 @@
-def pet_shop_name(title)
-return title[:name]
+def pet_shop_name(pet_shop)
+return pet_shop[:name]
 end
 
-def total_cash(money)
-  return money[:admin][:total_cash]
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(received, amount)
-  return received[:admin][:total_cash] += amount
+# the function below passes tests 3 and 4.
+def add_or_remove_cash(pet_shop, income)
+  return pet_shop[:admin][:total_cash] += income
 end
 
-def add_or_remove_cash(payout, amount)
-  return payout[:admin][:total_cash] += amount
-end
-
-def pets_sold(sales)
-  return sales[:admin][:pets_sold]
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
 end
 
 def increase_pets_sold(pet_shop,sales)
@@ -26,34 +23,25 @@ def stock_count(amount_of_stock)
   return amount_of_stock[:pets].count()
 end
 
-def pets_by_breed(pet_shop, dugtype)
+# this function passes tests 7 and 8.
+def pets_by_breed(pet_shop, pet_type)
   found_pets = [] # this creates an empty array.
   for pet in pet_shop[:pets] # this loops through the 6 hashes in the [:pets] array.
-    if pet[:breed] == dug_type # if any of those 6 hashes has a |breed:"British Shorthair"| pair, it will match the dug_type arguement.
+    if pet[:breed] == pet_type # if any of those 6 hashes has a |breed:"British Shorthair"| pair, it will match the dug_type arguement.
      found_pets.push(pet) # this will push any hashes containing the |breed:"British Shorthair"| pair into the found_pets array.
     end
   end
   return found_pets # this returns the array which contains 2 hashes.
 end
 
-def pets_by_breed(pet_shop, dugtype)
-    not_found_pets = []
-    for pet in pet_shop[:pets]
-      if pet[:breed] == dugtype
-       not_found_pets.push(pet)
-      end
-    end
-    return not_found_pets
-end
-
 def find_pet_by_name(pet_shop, pet_name)
-  for pet in pet_shop[:pets]
-    if pet[:name] == pet_name
-      return pet
-    end
+  for pet in pet_shop[:pets][:name]
+    return pet if pet == pet_name
   end
+  return nil
 end
 
+# Why can't I move [:name] from line 47 to line 46 as I did in the function above????? 
 def find_pet_by_name(pet_shop, pet_name)
   for pet in pet_shop[:pets]
     return pet if pet[:name] == pet_name
@@ -61,6 +49,7 @@ def find_pet_by_name(pet_shop, pet_name)
   return nil
 end
 
+# Why can't I move [:name] from line 55 to line 54
 def remove_pet_by_name(pet_shop, pet_name)
   for pet in pet_shop[:pets] #this loops through the 6 hashes in the [:pets] array.
     if pet[:name] == pet_name #if any of those 6 hashes has a name: "Arthur" pair, it will match the pet_name arguement.
@@ -74,15 +63,18 @@ def add_pet_to_stock(pet_shop, new_pet)
 end
 
 def customer_pet_count(customer)
-  for pet in customer
-    return pet.count(pet)
-  end
+  return customer[:pets].count
 end
-
-# 15 - can't get this to run
-# def add_pet_to_customer(customer, new_pet)
-#    return customer[:pets].push(new_pet)
+# This is another way of doing it.
+# def customer_pet_count(customer)
+#   for pet in customer
+#     return pet.count(pet)
+#   end
 # end
+
+def add_pet_to_customer(customer, new_pet)
+   return customer[:pets].push(new_pet)
+end
 
 
 # # OPTIONAL BELOW.
@@ -95,6 +87,13 @@ def customer_can_afford_pet(customer, new_pet)
     return false
   end
 end
+
+
+# def sell_pet_to_customer(pet_shop, pet, customer)
+
+  
+
+
 
 
 
